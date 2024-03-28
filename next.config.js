b47a9c34dc88aec60580904b87c218583c6f764b
ermiyas <128813243+ermi1999@users.noreply.github.com>
2024-03-29 00:29:50 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,28 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        // pathname: "/account123/**",
+      },
+    ],
+  },
   async redirects() {
     return [
       {
-        source: '/sign-in',
-        destination: '/api/auth/login',
+        source: "/sign-in",
+        destination: "/api/auth/login",
         permanent: true,
       },
       {
-        source: '/sign-up',
-        destination: '/api/auth/register',
+        source: "/sign-up",
+        destination: "/api/auth/register",
         permanent: true,
       },
-    ]
+    ];
   },
 
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, webpack }
-  ) => {
-    config.resolve.alias.canvas = false
-    config.resolve.alias.encoding = false
-    return config
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
